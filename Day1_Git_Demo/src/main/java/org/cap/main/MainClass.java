07ff968ef952e1a140fa4096b89c63e5bbf48027
ppaushya <ppaushya@din73003000.corp.capgemini.com>
2018-11-23 14:25:37 +0530

    First Program initiated

---
+++ Day1_Git_Demo/src/main/java/org/cap/main/MainClass.java
@@ -0,0 +1,19 @@
+package org.cap.main;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+	
+		System.out.println("Welcome Veere");
+		greetings();
+	
+	}
+
+	private static void greetings() {
+		System.out.println("Good Evening!");
+		
+	}
+
+	
+
+}
